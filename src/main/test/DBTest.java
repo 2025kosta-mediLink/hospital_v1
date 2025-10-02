da1f498edb66f8f71ab729b6542e12d76f2a97d3
Jeongwonwoo <zkjjx@naver.com>
2025-10-02 11:10:34 +0900

    feat(db): DB 연결 유틸 클래스 생성 및 간단 조회 테스트

---
+++ src/main/test/DBTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import common.DBConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBTest {
+  public static void main(String[] args) {
+    String sql = "SELECT member_id, name, login_id FROM member";
+
+    try (Connection conn = DBConnectionUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(sql);
+         ResultSet rs = pstmt.executeQuery()) {
+
+      System.out.println("DB 연결 성공!");
+
+      while (rs.next()) {
+        Long id = rs.getLong("member_id");
+        String name = rs.getString("name");
+        String loginId = rs.getString("login_id");
+
+        System.out.printf("id=%d, name=%s, loginId=%s%n", id, name, loginId);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
